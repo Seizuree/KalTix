a51db0964fa8c8517f005bc26bff082a0f1cdc16
Brian <briangiovanni03@gmail.com>
2024-03-13 11:56:51 +0700

    Initialize product contents

--- src/models/products/types.ts
+++ src/models/products/types.ts
@@ -1,6 +1,10 @@
 interface Product {
-  description: string
+  backdrop_path: string
+  genre_ids: number[]
   id: number
+  overview: string
+  poster_path: string
+  release_date: string
   title: string
 }
 
